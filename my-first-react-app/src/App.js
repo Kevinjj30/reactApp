c643648ed662fbea5db25f0a151a2438834f8773
kevinjj30 <kevinjenkins56@yahoo.com>
2020-01-12 21:56:03 -0600

    first iteration

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,26 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    );
+export default class App extends Component {
+
+//   constructor(props) {
+//       super(props);
+//       this.state = { items: [], text: '' };
+//       this.removeItem = this.removeItem.bind(this);
+//     }
+   
+  
+// 	 onChange = (event) => {
+// 		this.setState({ term: event.target.value });
+// 	} //allows me to enter text
+
+//   	onSubmit = (event) => {
+//     	event.preventDefault();
+// 		this.setState({
+// 			term: '',
+// 			items: [...this.state.items, this.state.term]
+// 		});
+//     }
+
+//     removeItem(index) {
+//       const items = this.state.items.filter((item, itemsIndex) => {
+//         return itemsIndex !== index
+//       })
+//       this.setState({ items })
+//     }
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<form className="App" onSubmit={this.onSubmit}>
+// 					<input value={this.state.term} onChange={this.onChange} />
+// 					<button>Submit</button>
+// 				</form>
+//         <List 
+//         items={this.state.items}
+//         removeItem={this.removeItem}
+//          />
+// 			</div>
+      
+// 		);
+// 	}
+// }
+
+
+constructor(props){
+  super(props);
+  this.state = {
+      todos: [],
+      todo: ''
+  }
+  this.changeTodo = this.changeTodo.bind(this);
+  this.addTodo = this.addTodo.bind(this);
+  this.removeTodo = this.removeTodo.bind(this);
 }
 
-export default App;
+changeTodo(event){
+  this.setState({
+      todo: event.target.value
+  })
+}
+
+addTodo(){
+  let { todo, todos } = this.state;
+  this.setState({
+      todo: '',
+      todos: [...todos, todo]
+  })
+}
+
+removeTodo(index){
+  let { todos } = this.state;
+  todos.splice(index, 1);
+  this.setState({
+      todos: todos
+  })
+}
+
+render(){
+  let { todo, todos } = this.state;
+  return <div>
+      <input value={todo} onChange={this.changeTodo}/>
+      <button onClick={this.addTodo}>Add Todo</button>
+      {
+          todos.map((todo, index)=>{
+              return <li onClick={this.removeTodo.bind(undefined, index)} key={index}>{todo}</li>
+          })
+      }
+  </div>
+}
+}
+
+
+// constructor(props) {
+//   super(props);
+//   this.state = { todos: [], text: '' };
+//   this.removeTodo = this.removeTodo.bind(this);
+// }
+
+// addTodo(e) {
+//   e.preventDefault();
+//   this.setState({ 
+//     todos: [ this.state.text, ...this.state.todos ],
+//     text: ''
+//   });
+// }
+
+// removeTodo(name, i){
+//   let todos = this.state.todos.slice();
+//   todos.splice(i, 1);
+//   this.setState({
+//       todos
+//   });
+// }
+
+// updateValue(e) {
+//   this.setState({ text: e.target.value})
+// }
+
+// render() {
+//   return(
+//       <div>
+//           <form onSubmit = {(e) => this.addTodo(e)}>
+//               <input
+//                   placeholder="Add Todo"
+//                   value={this.state.text}
+//                   onChange={(e) => {this.updateValue(e)}}
+//               />
+//               <button type="submit">Add Todo</button>
+//           </form>
+//           <TodoList todos={this.state.todos} removeTodo={this.removeTodo}/>
+//       </div>
+//   );
+// }
+// }
---
+++ my-first-react-app/src/components/List.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+
+
+// const List = props => (
+// 	<ul>
+// 	{
+// 		this.props.items.map((item, i) => (
+// 			<li key={i}>
+// 				{ false ? (
+// 				// { isActive ? (
+// 					<div>
+// 						<input value={item}></input><button></button>
+// 					</div>
+// 				):(
+// 					<div>
+// 						<span>{item} </span><button>EDIT</button><button onClick={(e) => { this.props.removeItem(i)}} key={item}>{ item }>DELETE</button>
+// 					</div>
+// 				) }
+// 			</li>
+// 		))
+// 	}
+// 	</ul>
+// );
+
+
+
+// export default List;
